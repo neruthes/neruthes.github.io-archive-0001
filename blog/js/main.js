e8f40b0224b5832c40623dd46261bea5995c46a3
Joy Neop <i@joyneop.com>
2014-02-08 00:13:31 +0800

    What if remove `purl.js` ?

--- blog/js/main.js
+++ blog/js/main.js
@@ -34,17 +34,24 @@ var postIndex = eval("(" + ajaxList.responseText + ")");
 
 var total = postIndex.list.length;
 var blogName = blogMetaData.blogName;
-var url = purl();
 
-var postId = url.param("p");
-var pageId = url.param("page");
+var pageId = "NULL";
+var postId = "NULL";
+
+var loc = window.location.href;
+
+if (loc.indexOf("?p=") != -1) {
+	postId = Number(loc.slice(loc.indexOf("?") + 3));
+} else if (loc.indexOf("?page=") != -1) {
+	pageId = Number(loc.slice(loc.indexOf("?") + 6));
+}
 
 var lastPage;
 var theLatestPostId = total-1;
 
-if (postId == undefined && pageId != undefined) {
-	var prevpage = Number(pageId)+1;
-	var nextpage = Number(pageId)-1;
+if (postId == "NULL" && pageId != "NULL") {
+	var prevpage = pageId+1;
+	var nextpage = pageId-1;
 	if (total%10 != 0) {
 		lastPage = (total-(total%10))/10+1;
 	} else {
@@ -65,28 +72,28 @@ if (postId == undefined && pageId != undefined) {
 		fillNav(prevpage, nextpage, 1, 1, "page");
 		loadPage(pageId, 10);
 	}
-} else if (postId != undefined && pageId == undefined) {
+} else if (postId != "NULL" && pageId == "NULL") {
 	if (blogMetaData.comments == "on") {
 		document.getElementById("comment-container").style.display = "block";
 		document.getElementById("comment-container").style.visibility = "visible";
 	}
-	var prevpost = Number(postId)-1;
-	var nextpost = Number(postId)+1;
-	if (Number(postId) < total && Number(postId) >= 0) {
+	var prevpost = postId-1;
+	var nextpost = postId+1;
+	if (postId < total && postId >= 0) {
 		loadPost(postId, 10, 1);
 	}
 	if (nextpost == total) {
 		fillNav(prevpost, nextpost, 1, 0, "p");
-	} else if (Number(postId) == 0) {
+	} else if (postId == 0) {
 		fillNav(prevpost, nextpost, 0, 1, "p");
-	} else if (Number(postId) < 0) {
+	} else if (postId < 0) {
 		httpError("404");
-	} else if (Number(postId) > theLatestPostId) {
+	} else if (postId > theLatestPostId) {
 		httpError("404");
 	} else {
 		fillNav(prevpost, nextpost, 1, 1, "p");
 	}
-} else if (postId == undefined && pageId == undefined) {
+} else if (postId == "NULL" && pageId == "NULL") {
 	loadPage(1, 10);
 	fillNav(2, 0, 1, 0, "page");
 }
