d4fa20affbabf32c75c9820f4ae39ba8058fa1ae
Joy Neop <i@joyneop.com>
2014-05-28 20:04:47 +0800

    alert script minor change

--- jn-alert-view.js
+++ jn-alert-view.js
@@ -6,7 +6,7 @@
 		position = "absolute";
 	}
 	if (style == undefined) {
-		style = "color: #FFF; padding: 8px 14px; background: rgba(128, 128, 128, 0.75); border: 1px solid #AAA; border-radius: 3px;";
+		style = "color: #FAFAFA; padding: 6px 14px; background: rgba(0, 0, 0, 0.273); border-radius: 3px; opacity: 1;";
 	}
 
 	// CSS
@@ -21,9 +21,10 @@
 	} else if (quadrant == 3) {
 		corner = "bottom: 15px; left: 15px;";
 	} else {
+		// if (quadrant == 4)
 		corner = "bottom: 15px; right: 15px;";
 	}
-	var preStyle = "#" + divId + " { position: " + position + "; " + corner + " max-width: 360px; font-size: 15px; font-weight: 300; } #" + divId + " span * { color: inherit; text-decoration: inherit; }";
+	var preStyle = "#" + divId + " { position: " + position + "; " + corner + " z-index: 9999; box-sizing: border-box; max-width: 378px; font-size: 15px; font-weight: 300; } #" + divId + " span * { color: inherit; text-decoration: inherit; }";
 	var es = "#JNALERTDIVID span:last-child { display: none; } #JNALERTDIVID:hover { opacity: 1; } #JNALERTDIVID:hover span:first-child { display: none; } #JNALERTDIVID:hover span:last-child { display: inline; }";
 	extraStyle = es.replace(/JNALERTDIVID/g, divId);
 	var styleTag = document.createElement("style");
@@ -40,4 +41,4 @@
 	div.appendChild(s1);
 	div.appendChild(s2);
 	document.body.appendChild(div);
-})("I'm seeking 2015 summer internship...", "<a href='http://neopstudio.github.io/resume/'>Does your company have an internship program? Maybe I can be one in your company this summer? Click here to see my resume, thanks : )</a>", 1, "absolute", "color: #FDFDFD; padding: 8px 14px; background: rgba(0, 0, 0, 0.55); border-radius: 3px; box-shadow: rgba(0, 0, 0, 0.35) 0 2px 10px; opacity: 0.7");
+})("I'm seeking 2015 summer internship...", "<a href='http://neopstudio.github.io/resume/' target='_blank'>Does your company have an internship program? Maybe I can be one in your company this summer? Click here to see my resume, thanks : )</a>", 4, "absolute");
