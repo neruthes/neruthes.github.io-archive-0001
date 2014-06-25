113055288824787ecad748b1fd0da6204b80dedb
Joy Neop <i@joyneop.com>
2014-06-25 12:15:59 +0800

    multi-language support

--- blog/js/func.js
+++ blog/js/func.js
@@ -18,94 +18,96 @@ Copyright info:
 ----------------------------------------*/
 
 function createSection(pid) {
-	var section = document.createElement("section")
-	var h2 = document.createElement("h2")
-	var h2a = document.createElement("a")
-	var div = document.createElement("div")
-	var footer = document.createElement("footer")
-	var dateAndTime = document.createElement("a")
-	section.setAttribute("class", "post")
-	section.setAttribute("id", "post" + pid)
-	h2.setAttribute("id", "post" + pid + "h2")
-	h2a.setAttribute("id", "post" + pid + "title")
-	div.setAttribute("class", "post-text")
-	div.setAttribute("id", "post" + pid + "text")
-	dateAndTime.setAttribute("id", "post" + pid + "link")
-	section.appendChild(h2)
-	section.appendChild(div)
-	section.appendChild(footer)
-	h2.appendChild(h2a)
-	footer.appendChild(dateAndTime)
-	cont.appendChild(section)
-	sectionsCreated++
+	var section = document.createElement("section");
+	var h2 = document.createElement("h2");
+	var h2a = document.createElement("a");
+	var div = document.createElement("div");
+	var footer = document.createElement("footer");
+	var dateAndTime = document.createElement("a");
+	section.setAttribute("class", "post");
+	section.setAttribute("id", "post" + pid);
+	h2.setAttribute("id", "post" + pid + "h2");
+	h2a.setAttribute("id", "post" + pid + "title");
+	div.setAttribute("class", "post-text");
+	div.setAttribute("id", "post" + pid + "text");
+	dateAndTime.setAttribute("id", "post" + pid + "link");
+	section.appendChild(h2);
+	section.appendChild(div);
+	section.appendChild(footer);
+	h2.appendChild(h2a);
+	footer.appendChild(dateAndTime);
+	cont.appendChild(section);
+	sectionsCreated++;
 }
 
 function loadPost(pid) {
-	var ajaxContent = new XMLHttpRequest()
-	ajaxContent.open("GET", "db/" + pid + ".txt", false)
-	ajaxContent.send()
-	var receivedPostText = ajaxContent.responseText
+	var ajaxContent = new XMLHttpRequest();
+	ajaxContent.open("GET", "db/" + pid + ".txt", false);
+	ajaxContent.send();
+	var receivedPostText = ajaxContent.responseText;
 
 	if (postIndex.list[pid].Title == "") {
-		document.getElementById("post" + pid + "h2").style.display = "none"
+		document.getElementById("post" + pid + "h2").style.display = "none";
 	} else {
-		document.getElementById("post" + pid + "title").innerHTML = postIndex.list[pid].Title
-		document.getElementById("post" + pid + "title").href = "./?p=" + pid
+		document.getElementById("post" + pid + "title").innerHTML = postIndex.list[pid].Title;
+		document.getElementById("post" + pid + "title").href = "./?p=" + pid;
 	}
 	if (receivedPostText == "") {
-		document.getElementById("post" + pid + "text").style.display = "none"
+		document.getElementById("post" + pid + "text").style.display = "none";
 	} else {
-		document.getElementById("post" + pid + "text").innerHTML = receivedPostText
+		document.getElementById("post" + pid + "text").innerHTML = receivedPostText;
 	}
-	document.getElementById("post" + pid + "link").innerHTML = postIndex.list[pid].Time
-	document.getElementById("post" + pid + "link").href = "./?p=" + pid
+	document.getElementById("post" + pid + "h2").lang = postIndex.list[pid].Lang[0];
+	document.getElementById("post" + pid + "text").lang = postIndex.list[pid].Lang[1];
+	document.getElementById("post" + pid + "link").innerHTML = postIndex.list[pid].Time;
+	document.getElementById("post" + pid + "link").href = "./?p=" + pid;
 	if (postId != "NULL" && postIndex.list[pid].Title != "") {
-		document.title = postIndex.list[pid].Title + " — " + blogName
+		document.title = postIndex.list[pid].Title + " — " + blogName;
 	} else if (postId != "NULL" && postIndex.list[pid].Title == "") {
-		document.title = "Post #" + pid + " — " + blogName
+		document.title = "Post #" + pid + " — " + blogName;
 	} else {
-		document.title = blogName
+		document.title = blogName;
 	}
-	loadedOldestPostId--
+	loadedOldestPostId--;
 }
 
 function loadMore() {
 	if (postId == "NULL" && document.height-window.pageYOffset-window.innerHeight < 900) {
-		loadMultiPosts(loadedOldestPostId-1)
+		loadMultiPosts(loadedOldestPostId-1);
 	}
 }
 
 function loadMultiPosts(pid) {
 	for (var i = pid; i > pid-10 && loadedOldestPostId != 0; i--) {
-		createSection(i)
-		loadPost(i)
+		createSection(i);
+		loadPost(i);
 	}
 }
 
 function fillNav(previd, nextid, ifprev, ifnext) {
 	if (ifprev == 1) {
-		document.getElementById("prevlink").href = "./?p=" + previd
-		document.getElementById("prevlink").innerHTML = "&lt; Prev"
-		document.getElementById("prevlink").style.visibility = "visible"
+		document.getElementById("prevlink").href = "./?p=" + previd;
+		document.getElementById("prevlink").innerHTML = "&lt; Prev";
+		document.getElementById("prevlink").style.visibility = "visible";
 	}
 	if (ifnext == 1) {
 		if (nextid == 1) {
-			document.getElementById("nextlink").href = "./"
+			document.getElementById("nextlink").href = "./";
 		} else {
-			document.getElementById("nextlink").href = "./?p=" + nextid
+			document.getElementById("nextlink").href = "./?p=" + nextid;
 		}
-		document.getElementById("nextlink").innerHTML = "Next &gt;"
-		document.getElementById("nextlink").style.visibility = "visible"
+		document.getElementById("nextlink").innerHTML = "Next &gt;";
+		document.getElementById("nextlink").style.visibility = "visible";
 	}
 }
 
 function httpError() {
-	createSection(postId)
-	document.getElementById("post" + postId + "title").innerHTML = "404 Not Found : ("
-	document.getElementById("post" + postId + "title").style.display = "block"
-	document.getElementById("post" + postId + "link").style.display = "none"
-	document.getElementById("prevandnext").innerHTML = ""
-	document.getElementById("comment-container").style.display = "none"
-	document.title = "HTTP 404 " + "— " + blogName
-	document.getElementById("post" + postId + "text").innerHTML = "Here is nothing you can see. Back to <em><a href='./'>homepage</a></em>?"
+	createSection(postId);
+	document.getElementById("post" + postId + "title").innerHTML = "404 Not Found : (";
+	document.getElementById("post" + postId + "title").style.display = "block";
+	document.getElementById("post" + postId + "link").style.display = "none";
+	document.getElementById("prevandnext").innerHTML = "";
+	document.getElementById("comment-container").style.display = "none";
+	document.title = "HTTP 404 " + "— " + blogName;
+	document.getElementById("post" + postId + "text").innerHTML = "Here is nothing you can see. Back to <em><a href='./'>homepage</a></em>?";
 }
--- blog/js/main.js
+++ blog/js/main.js
@@ -17,60 +17,60 @@ Copyright info:
 	License Link: http://creativecommons.org/licenses/by-nc-sa/4.0/deed.en_US
 ----------------------------------------*/
 
-var ajaxMetaData = new XMLHttpRequest()
-ajaxMetaData.open("GET", "./meta.json", false)
-ajaxMetaData.send()
-var blogMetaData = JSON.parse(ajaxMetaData.responseText)
+var ajaxMetaData = new XMLHttpRequest();
+ajaxMetaData.open("GET", "./meta.json", false);
+ajaxMetaData.send();
+var blogMetaData = JSON.parse(ajaxMetaData.responseText);
 
-var ajaxList = new XMLHttpRequest()
-ajaxList.open("GET", "./list.json", false)
-ajaxList.send()
-var postIndex = JSON.parse(ajaxList.responseText)
+var ajaxList = new XMLHttpRequest();
+ajaxList.open("GET", "./list.json", false);
+ajaxList.send();
+var postIndex = JSON.parse(ajaxList.responseText);
 
-var total = postIndex.list.length
-var blogName = blogMetaData.blogName
+var total = postIndex.list.length;
+var blogName = blogMetaData.blogName;
 
-var postId = "NULL"
+var postId = "NULL";
 
-var loc = window.location.href
+var loc = window.location.href;
 
 if (loc.indexOf("?p=") != -1) {
-	postId = Number(loc.slice(loc.indexOf("?") + 3))
+	postId = Number(loc.slice(loc.indexOf("?") + 3));
 }
 
-var lastPage
-var theLatestPostId = total-1
-var loadedOldestPostId = total
-var sectionsCreated = 0
+var lastPage;
+var theLatestPostId = total-1;
+var loadedOldestPostId = total;
+var sectionsCreated = 0;
 
-var cont = document.getElementById("cont")
+var cont = document.getElementById("cont");
 
 if (postId != "NULL") {
 	if (blogMetaData.comments == "on") {
-		document.getElementById("comment-container").style.display = "block"
-		document.getElementById("comment-container").style.visibility = "visible"
+		document.getElementById("comment-container").style.display = "block";
+		document.getElementById("comment-container").style.visibility = "visible";
 	}
-	var prevpost = postId-1
-	var nextpost = postId+1
+	var prevpost = postId-1;
+	var nextpost = postId+1;
 	if (postId < total && postId >= 0) {
-		createSection(postId)
-		loadPost(postId)
+		createSection(postId);
+		loadPost(postId);
 	}
 	if (nextpost == total) {
-		fillNav(prevpost, nextpost, 1, 0)
+		fillNav(prevpost, nextpost, 1, 0);
 	} else if (postId == 0) {
-		fillNav(prevpost, nextpost, 0, 1)
+		fillNav(prevpost, nextpost, 0, 1);
 	} else if (postId < 0) {
-		httpError()
+		httpError();
 	} else if (postId > theLatestPostId) {
-		httpError()
+		httpError();
 	} else {
-		fillNav(prevpost, nextpost, 1, 1)
+		fillNav(prevpost, nextpost, 1, 1);
 	}
 } else if (postId == "NULL") {
 	for (var i = total - 1; i >= total-10 && i >= 0; i--) {
-		createSection(i)
-		loadPost(i)
+		createSection(i);
+		loadPost(i);
 	}
-	loadMultiPosts(loadedOldestPostId-1)
+	loadMultiPosts(loadedOldestPostId-1);
 }
