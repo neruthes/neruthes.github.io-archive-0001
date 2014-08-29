13ea7c1a37ba39c7af9f0b02bc6d59fca5d887c6
Joy Neop <joyneop@me.com>
2014-08-30 00:03:57 +0800

    `jn-alert-view.js` minor update

--- jn-alert-view.js
+++ jn-alert-view.js
@@ -3,11 +3,9 @@
 	@Author Joy Neop
 	@Website http://www.joyneop.com/
 	@GitHub https://github.com/JoyNeop
-	@Twitter https://twitter.com/JoyNeop
-	@Email i@joyneop.com
 	© 2014 Joy Neop. All rights reserved.
 	*/
-	window.jnalert = {};
+	var jnalert = {};
 
 	if (quadrant == undefined || quadrant == null) {
 		quadrant = 1;
@@ -24,8 +22,8 @@
 
 	// CSS
 	var sizeText = [ "width: " + size[0] + "px; height: " + size[1] + "px; ", "width: " + size[2] + "px; height: " + size[3] + "px; "];
-	var divId = "jn-alert-3389";
-	window.jnalert.divId = "jn-alert-3389";
+	var divId = "jn-alert-3389-C277-D9IR";
+	jnalert.divId = "jn-alert-3389-C277-D9IR";
 	var styleText = "#" + divId + " { " + style + " }";
 	var corner;
 	if (quadrant == 1) {
@@ -38,12 +36,12 @@
 		// if (quadrant == 4)
 		corner = "bottom: 15px; right: 15px;";
 	}
-	var preStyle = "#jn-alert-3389 { position: " + position + "; " + corner + sizeText[0] + " z-index: 9999; box-sizing: border-box; max-width: 390px; font-size: 15px; font-weight: 300; line-height: 20px; letter-spacing: 0; overflow: hidden; -webkit-transition: all 300ms ease; -moz-transition: all 300ms ease; transition: all 300ms ease; } #jn-alert-3389 span { padding: 6px 14px; box-sizing: border-box; } #jn-alert-3389 span * { color: inherit; text-decoration: inherit; }";
-	var extraStyle = "#jn-alert-3389 span:first-child { display: block; } #jn-alert-3389 span:last-child { display: none; } #jn-alert-3389:hover { opacity: 1; " + sizeText[1] + " } #jn-alert-3389:hover span:first-child { display: none; } #jn-alert-3389:hover span:last-child { display: block; }";
+	var preStyle = "#jn-alert-3389-C277-D9IR { position: " + position + "; " + corner + sizeText[0] + " z-index: 9999; box-sizing: border-box; max-width: 390px; font-size: 15px; font-weight: 300; line-height: 20px; letter-spacing: 0; overflow: hidden; -webkit-transition: all 300ms ease; -moz-transition: all 300ms ease; transition: all 300ms ease; } #jn-alert-3389-C277-D9IR span { padding: 6px 14px; box-sizing: border-box; } #jn-alert-3389-C277-D9IR span * { color: inherit; text-decoration: inherit; }";
+	var extraStyle = "#jn-alert-3389-C277-D9IR span:first-child { display: block; } #jn-alert-3389-C277-D9IR span:last-child { display: none; } #jn-alert-3389-C277-D9IR:hover { opacity: 1; " + sizeText[1] + " } #jn-alert-3389-C277-D9IR:hover span:first-child { display: none; } #jn-alert-3389-C277-D9IR:hover span:last-child { display: block; }";
 	var styleTag = document.createElement("style");
 	styleTag.setAttribute("id", divId + "style");
-	window.jnalert.styleTagContent = preStyle + styleText + extraStyle;
-	styleTag.appendChild(document.createTextNode(window.jnalert.styleTagContent));
+	jnalert.styleTagContent = preStyle + styleText + extraStyle;
+	styleTag.appendChild(document.createTextNode(jnalert.styleTagContent));
 	document.getElementsByTagName("head")[0].appendChild(styleTag);
 
 	// DOM
@@ -60,14 +58,18 @@
 	document.body.appendChild(div);
 
 	// Time
-	window.jnalert.show = function () {
-		document.getElementById(window.jnalert.divId + "style").innerHTML = window.jnalert.styleTagContent + "#" + window.jnalert.divId + " { opacity: 1; }";
-	};
-	window.jnalert.dim = function () {
-		document.getElementById(window.jnalert.divId + "style").innerHTML = window.jnalert.styleTagContent + "#" + window.jnalert.divId + " { opacity: 0.05; }";
-	};
-	window.setTimeout(window.jnalert.show, 4);
-	window.setTimeout(window.jnalert.dim, 2200);
+	window.setTimeout(function(){
+		document.getElementById(jnalert.divId + "style").innerHTML = jnalert.styleTagContent + "#" + jnalert.divId + " { opacity: 1; }";
+	}, 4);
+	window.setTimeout(function(){
+		document.getElementById(jnalert.divId + "style").innerHTML = jnalert.styleTagContent + "#" + jnalert.divId + " { opacity: 0.0655555; }";
+	}, 2200);
 	console.log(alert);
 	console.log(text);
-})("Hi, I'm seeking 2015 summer internship...", "<a href='http://neopstudio.github.io/resume/' style='display: block; width: 364px;' target='_blank'>Does your company have an internship program? Maybe I can be one in your company this summer? Click here to see my résumé, thanks : )</a>", 1, "absolute", null, [ 320, 32, 390, 72 ]);
+})("Hi, I'm seeking 2015 summer internship...",
+	'<a href="http://www.joyneop.com/resume/" style="display: block; width: 364px;" target="_blank">Does your company have an internship program? Maybe I can be one in your company this summer? Click here to see my résumé, thanks : )</a>',
+	1,
+	"absolute",
+	null,
+	[ 320, 32, 390, 72 ]
+);
