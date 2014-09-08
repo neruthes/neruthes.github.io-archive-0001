65a8c189c2a035d9590526405ae3e93d3be0dcb3
Joy Neop <joyneop@gmail.com>
2014-09-08 09:29:13 -0700

    test 2

--- jn-alert-view.js
+++ jn-alert-view.js
@@ -37,7 +37,7 @@
 		corner = "bottom: 15px; right: 15px;";
 	}
 	var preStyle = "#jn-alert-3389-C277-D9IR { position: " + position + "; " + corner + sizeText[0] + " z-index: 9999; box-sizing: border-box; max-width: 390px; font-size: 15px; font-weight: 300; line-height: 20px; letter-spacing: 0; overflow: hidden; -webkit-transition: all 353ms ease; -moz-transition: all 353ms ease; transition: all 353ms ease; } #jn-alert-3389-C277-D9IR span { padding: 6px 14px; box-sizing: border-box; } #jn-alert-3389-C277-D9IR span * { color: inherit; text-decoration: inherit; }";
-	var extraStyle = "#jn-alert-3389-C277-D9IR span:first-child { display: inline; text-align: right; width: auto; } #jn-alert-3389-C277-D9IR span:last-child { display: none; } #jn-alert-3389-C277-D9IR:hover { background: rgba(0, 0, 0, 0.52312); opacity: 1; " + sizeText[1] + " } #jn-alert-3389-C277-D9IR:hover span:first-child { display: none; } #jn-alert-3389-C277-D9IR:hover span:last-child { display: block; }";
+	var extraStyle = "#jn-alert-3389-C277-D9IR span:first-child { display: block; text-align: right; } #jn-alert-3389-C277-D9IR span:last-child { display: none; } #jn-alert-3389-C277-D9IR:hover { background: rgba(0, 0, 0, 0.52312); opacity: 1; " + sizeText[1] + " } #jn-alert-3389-C277-D9IR:hover span:first-child { display: none; } #jn-alert-3389-C277-D9IR:hover span:last-child { display: block; }";
 	var styleTag = document.createElement("style");
 	styleTag.setAttribute("id", divId + "style");
 	jnalert.styleTagContent = preStyle + styleText + extraStyle;
