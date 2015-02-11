6dab879a74b7a3252a0fb7d4b5faeea915e16003
Joy Neop <joyneop@gmail.com>
2015-02-11 16:47:01 +0000

    add robots.txt

--- wiki/js/main.js
+++ wiki/js/main.js
@@ -17,114 +17,114 @@ Copyright info:
 	License Link: http://creativecommons.org/licenses/by-nc-sa/4.0/deed.en_US
 ----------------------------------------*/
 
-var postId = "NULL"
-var blogName = ""
-var loc = "_"
-var ajaxMetaData = new XMLHttpRequest()
-ajaxMetaData.open("GET", "./meta.json", false)
-ajaxMetaData.send()
-var blogMetaData = JSON.parse(ajaxMetaData.responseText)
-blogName = blogMetaData.blogName
+var postId = "NULL";
+var blogName = "";
+var loc = "_";
+var ajaxMetaData = new XMLHttpRequest();
+ajaxMetaData.open("GET", "./meta.json", false);
+ajaxMetaData.send();
+var blogMetaData = JSON.parse(ajaxMetaData.responseText);
+blogName = blogMetaData.blogName;
 
 function loadPost(pid) {
-	var ajaxContent = new XMLHttpRequest()
-	ajaxContent.open("GET", "db/" + pid + ".txt", false)
-	ajaxContent.send()
+	var ajaxContent = new XMLHttpRequest();
+	ajaxContent.open("GET", "db/" + pid + ".txt", false);
+	ajaxContent.send();
 	if (ajaxContent.status != 404) {
-		var receivedPostText = ajaxContent.responseText
+		var receivedPostText = ajaxContent.responseText;
 	} else {
-		var ajaxNotFound = new XMLHttpRequest()
-		ajaxNotFound.open("GET", "nw/NeopWiki404.txt", false)
-		ajaxNotFound.send()
-		var receivedPostText = ajaxNotFound.responseText
-	}
-	document.getElementById("wiki-h2").innerHTML = decodeURI(pid.replace(/_/g, " "))
-	document.title = decodeURI(pid.replace(/_/g, " ") + " — " + blogName)
+		var ajaxNotFound = new XMLHttpRequest();
+		ajaxNotFound.open("GET", "nw/NeopWiki404.txt", false);
+		ajaxNotFound.send();
+		var receivedPostText = ajaxNotFound.responseText;
+	};
+	document.getElementById("wiki-h2").innerHTML = decodeURI(pid.replace(/_/g, " "));
+	document.title = decodeURI(pid.replace(/_/g, " ") + " — " + blogName);
 
-	var middle = receivedPostText
+	var middle = receivedPostText;
 	while (middle.indexOf("\n\n\n") != -1) {
-		middle = middle.replace("\n\n\n", "\n\n")
-	}
+		middle = middle.replace("\n\n\n", "\n\n");
+	};
 	while (middle.indexOf("\n\n") == 0) {
-		middle = middle.replace("\n\n", "")
-	}
-	var arr = middle.split("\n\n")
-	var wikiText = document.getElementById("wiki-text")
-	wikiText.innerHTML = ""
+		middle = middle.replace("\n\n", "");
+	};
+	var arr = middle.split("\n\n");
+	var wikiText = document.getElementById("wiki-text");
+	wikiText.innerHTML = "";
 	for (var i = 0; i < arr.length; i++) {
-		var p
-		var wikiHeading = arr[i]
+		var p;
+		var wikiHeading = arr[i];
 		if (wikiHeading.indexOf("# ") == 0) {
-			p = document.createElement("h3")
-			wikiHeading = wikiHeading.replace("# ", "")
+			p = document.createElement("h3");
+			wikiHeading = wikiHeading.replace("# ", "");
 		} else if (wikiHeading.indexOf("## ") == 0) {
-			p = document.createElement("h4")
-			wikiHeading = wikiHeading.replace("## ", "")
+			p = document.createElement("h4");
+			wikiHeading = wikiHeading.replace("## ", "");
 		} else if (wikiHeading.indexOf("### ") == 0) {
-			p = document.createElement("h5")
-			wikiHeading = wikiHeading.replace("### ", "")
+			p = document.createElement("h5");
+			wikiHeading = wikiHeading.replace("### ", "");
 		} else if (wikiHeading.indexOf("---") == 0 || wikiHeading.indexOf("* * *") == 0) {
-			p = document.createElement("hr")
-			wikiHeading = ""
+			p = document.createElement("hr");
+			wikiHeading = "";
 		} else if (wikiHeading.indexOf(" ") == 0) {
-			p = document.createElement("pre")
-			wikiHeading = wikiHeading.slice(1)
+			p = document.createElement("pre");
+			wikiHeading = wikiHeading.slice(1);
 		} else if (wikiHeading.indexOf("> ") == 0) {
 			p = document.createElement("blockquote")
-			wikiHeading = wikiHeading.slice(2)
+			wikiHeading = wikiHeading.slice(2);
 		} else if (wikiHeading.indexOf("- ") == 0) {
-			p = document.createElement("ul")
-			var list = wikiHeading.split("\n")
-			wikiHeading = ""
+			p = document.createElement("ul");
+			var list = wikiHeading.split("\n");
+			wikiHeading = "";
 			for (var j = 0; j < list.length; j++) {
-				list[j] = list[j].slice(2)
-				wikiHeading = wikiHeading + "<li>" + list[j] + "</li>"
-			}
+				list[j] = list[j].slice(2);
+				wikiHeading = wikiHeading + "<li>" + list[j] + "</li>";
+			};
 		} else {
-			p = document.createElement("p")
-		}
-		var wikiTemplates = wikiHeading
+			p = document.createElement("p");
+		};
+		var wikiTemplates = wikiHeading;
 		while (wikiTemplates.indexOf("{{") != -1) {
-			var tempUrl = wikiTemplates.slice((wikiTemplates.indexOf("{{") + 2), wikiTemplates.indexOf("}}"))
-			var ajaxTemplate = new XMLHttpRequest()
-			ajaxTemplate.open("GET", "tem/" + tempUrl.replace(/ /g, "_") + ".txt", false)
-			ajaxTemplate.send()
+			var tempUrl = wikiTemplates.slice((wikiTemplates.indexOf("{{") + 2), wikiTemplates.indexOf("}}"));
+			var ajaxTemplate = new XMLHttpRequest();
+			ajaxTemplate.open("GET", "tem/" + tempUrl.replace(/ /g, "_") + ".txt", false);
+			ajaxTemplate.send();
 			if (ajaxTemplate.status != 404) {
-				var rawTemplateText = ajaxTemplate.responseText
+				var rawTemplateText = ajaxTemplate.responseText;
 			} else {
-				var ajaxNotFound = new XMLHttpRequest()
-				ajaxNotFound.open("GET", "nw/NeopWiki404.txt", false)
-				ajaxNotFound.send()
-				var rawTemplateText = ajaxNotFound.responseText
-			}
-			wikiTemplates = wikiTemplates.replace("{{" + tempUrl, rawTemplateText).replace("}}", '')
-		}
-		var wikiLinks = wikiTemplates
+				var ajaxNotFound = new XMLHttpRequest();
+				ajaxNotFound.open("GET", "nw/NeopWiki404.txt", false);
+				ajaxNotFound.send();
+				var rawTemplateText = ajaxNotFound.responseText;
+			};
+			wikiTemplates = wikiTemplates.replace("{{" + tempUrl, rawTemplateText).replace("}}", '');
+		};
+		var wikiLinks = wikiTemplates;
 		while (wikiLinks.indexOf("[[") != -1) {
-			var linkUrl = wikiLinks.slice((wikiLinks.indexOf("[[") + 2), wikiLinks.indexOf("]]"))
+			var linkUrl = wikiLinks.slice((wikiLinks.indexOf("[[") + 2), wikiLinks.indexOf("]]"));
 			if (linkUrl.indexOf("|") == -1) {
-				wikiLinks = wikiLinks.replace("[[", '<a href="#' + linkUrl.replace(/ /g, "_") + '">').replace("]]", '</a>')
+				wikiLinks = wikiLinks.replace("[[", '<a href="#' + linkUrl.replace(/ /g, "_") + '">').replace("]]", '</a>');
 			} else {
-				var wikiLinkText = wikiLinks.slice(wikiLinks.indexOf("|") + 1, wikiLinks.indexOf("]]"))
-				var wikiLinkHref = wikiLinks.slice(wikiLinks.indexOf("[[") + 2, wikiLinks.indexOf("|"))
-				wikiLinks = wikiLinks.replace("[[", '<a href="#' + wikiLinkHref.replace(/ /g, "_") + '">').replace(wikiLinkHref + "|" + wikiLinkText + "]]", wikiLinkText + '</a>')
-			}
-		}
-		p.innerHTML = wikiLinks
-		wikiText.appendChild(p)
-	}
-}
+				var wikiLinkText = wikiLinks.slice(wikiLinks.indexOf("|") + 1, wikiLinks.indexOf("]]"));
+				var wikiLinkHref = wikiLinks.slice(wikiLinks.indexOf("[[") + 2, wikiLinks.indexOf("|"));
+				wikiLinks = wikiLinks.replace("[[", '<a href="#' + wikiLinkHref.replace(/ /g, "_") + '">').replace(wikiLinkHref + "|" + wikiLinkText + "]]", wikiLinkText + '</a>');
+			};
+		};
+		p.innerHTML = wikiLinks;
+		wikiText.appendChild(p);
+	};
+};
 
 function loadWiki() {
-	loc = window.location.href
+	loc = window.location.href;
 	if (loc.indexOf("#") != -1) {
-		postId = loc.slice(loc.indexOf("#") + 1)
-		loadPost(postId.split("")[0].toUpperCase() + postId.substring(1))
+		postId = loc.slice(loc.indexOf("#") + 1);
+		loadPost(postId.split("")[0].toUpperCase() + postId.substring(1));
 	} else {
-		window.location.replace(window.location.href + "#Main_Page")
-	}
-	scrollTo(0, 0)
+		window.location.replace(window.location.href + "#Main_Page");
+	};
+	scrollTo(0, 0);
 }
 
-loadWiki()
-window.onhashchange = loadWiki
+loadWiki();
+window.onhashchange = loadWiki;
