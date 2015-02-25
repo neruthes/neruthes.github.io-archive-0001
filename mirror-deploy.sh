8fcab3489dad00b80b624960e38e9e4aa7d58179
Joy Neop <joyneop@gmail.com>
2015-02-25 06:36:00 +0000

    done mirror-deploy.sh

--- mirror-deploy.sh
+++ mirror-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# cd "~/Developer/Parse-Products/joyneop-website-mirror/public"
-# git pull
-echo pulled
+cp -r ~/Developer/joyneop.github.io ~/Developer/Parse-Products/joyneop-website-mirror/public
+cd ~/Developer/Parse-Products/joyneop-website-mirror/public
+parse deploy
+echo Done it.
 exit 0
