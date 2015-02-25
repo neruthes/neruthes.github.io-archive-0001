3365358b713dc6849d3c46f67f8b15086ec3a82b
Joy Neop <joyneop@gmail.com>
2015-02-25 07:09:38 +0000

    mirror-deploy.sh bug fix 2

--- mirror-deploy.sh
+++ mirror-deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-cp -r ~/Developer/joyneop.github.io ~/Developer/Parse-Products/joyneop-website-mirror/public
-cd ~/Developer/Parse-Products/joyneop-website-mirror/public
+cp -r ~/Developer/joyneop.github.io ~/Developer/Parse-Products/joyneop-website-mirror/
+cd ~/Developer/Parse-Products/joyneop-website-mirror
+rm -rf public
+mv joyneop.github.io public
 parse deploy
 echo Done it.
 exit 0
