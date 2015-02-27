fb29cf00e8d9ff4e880f7513801bc01251847d6f
Joy Neop <joyneop@gmail.com>
2015-02-27 07:22:12 +0000

    ignore robots.txt

--- blog/feed.js
+++ blog/feed.js
@@ -8,7 +8,7 @@ Run on Node.js
 
 var fs = require('fs');
 
-var xmlTemplate = '<?xml version="1.0" encoding="UTF-8"?>\n\
+var xmlTemplate = '<?xml version="1.0" encoding="UTF-8" ?>\n\
 <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/"\n\
 xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/"\n\
 xmlns:atom="http://www.w3.org/2005/Atom" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"\n\
