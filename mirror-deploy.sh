75ad6b539014344d56c76c04515445305c451b0f
Joy Neop <joyneop@gmail.com>
2015-02-28 05:33:39 +0000

    serve robots.txt another way

--- mirror-deploy.sh
+++ mirror-deploy.sh
@@ -3,6 +3,8 @@ cp -r ~/Developer/joyneop.github.io ~/Developer/Parse-Products/joyneop-website-m
 cd ~/Developer/Parse-Products/joyneop-website-mirror
 rm -rf public
 mv joyneop.github.io public
+cd public
+mv _robots.txt robots.txt
 parse deploy
 echo Done it.
 exit 0
