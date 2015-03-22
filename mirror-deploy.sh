1db7efdde9461a06709bac39c97056efcaa01ba0
Joy Neop <joyneop@gmail.com>
2015-03-22 09:13:47 +0000

    something new

--- mirror-deploy.sh
+++ mirror-deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
+
+rm -rf ~/Developer/neopstudio.github.op/JN-Lab/other-images
+cp -r ~/Developer/joyneop.github.io/JN-Lab/other-images ~/Developer/neopstudio.github.io/JN-Lab/
+
+rm -rf ~/Developer/Parse-Products/joyneop-website-mirror/public
 cp -r ~/Developer/joyneop.github.io ~/Developer/Parse-Products/joyneop-website-mirror/
 cd ~/Developer/Parse-Products/joyneop-website-mirror
-rm -rf public
 mv joyneop.github.io public
 cd public
 mv _robots.txt robots.txt
+
 parse deploy
 echo Done it.
 exit 0
