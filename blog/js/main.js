c1e31243ebe7d8aca2e7ffcc9f9dab35fe2e08f2
Joy Neop <joyneop@gmail.com>
2015-04-03 13:07:58 +0000

    bug fix

--- blog/js/main.js
+++ blog/js/main.js
@@ -1,23 +1,24 @@
 var createSection = function (pid) {
-	var section = document.createElement("section");
-	var h2 = document.createElement("h2");
-	var h2a = document.createElement("a");
-	var div = document.createElement("div");
-	var footer = document.createElement("footer");
-	var dateAndTime = document.createElement("a");
-	section.setAttribute("class", "post");
-	section.setAttribute("id", "post" + pid);
-	h2.setAttribute("id", "post" + pid + "h2");
-	h2.setAttribute("class", "post-h2");
-	h2a.setAttribute("id", "post" + pid + "title");
-	h2a.setAttribute("class", "post-h2-in");
-	div.setAttribute("class", "post-text");
-	div.setAttribute("id", "post" + pid + "text");
-	dateAndTime.setAttribute("id", "post" + pid + "link");
+	var section = document.createElement('section');
+	var h2 = document.createElement('h2');
+	var h2a = document.createElement('a');
+	var div = document.createElement('div');
+	var footer = document.createElement('footer');
+	var dateAndTime = document.createElement('a');
+	section.setAttribute('class', 'post');
+	section.setAttribute('id', 'post' + pid);
+	h2.setAttribute('id', 'post' + pid + 'h2');
+	h2.setAttribute('class', 'post-h2');
+	h2a.setAttribute('id', 'post' + pid + 'title');
+	h2a.setAttribute('class', 'post-h2-in');
+	div.setAttribute('class', 'post-text');
+	div.setAttribute('id', 'post' + pid + 'text');
+	dateAndTime.setAttribute('id', 'post' + pid + 'link');
 	section.appendChild(h2);
 	section.appendChild(footer);
 	section.appendChild(div);
 	h2.appendChild(h2a);
+	footer.setAttribute('class', 'post-footer')
 	footer.appendChild(dateAndTime);
 	cont.appendChild(section);
 	sectionsCreated++;
@@ -26,29 +27,27 @@ var createSection = function (pid) {
 var loadPost = function (pid) {
 	var thisId = sectionsCreated-1;
 	ajaxContent[thisId] = new XMLHttpRequest();
-	ajaxContent[thisId].open("GET", "db/" + pid + ".txt", true);
+	ajaxContent[thisId].open('GET', 'db/' + pid + '.txt', true);
 	ajaxContent[thisId].send();
 	ajaxContent[thisId].onload = function () {
 		var receivedPostText = ajaxContent[thisId].responseText;
-		if (postIndex.list[pid].Title == "") {
-			document.getElementById("post" + pid + "h2").remove();
+		if (postIndex.list[pid].T == '') {
+			document.getElementById('post' + pid + 'h2').remove();
 		} else {
-			document.getElementById("post" + pid + "h2").lang = postIndex.list[pid].Lang[0];
-			document.getElementById("post" + pid + "title").innerHTML = postIndex.list[pid].Title;
-			document.getElementById("post" + pid + "title").href = "./?p=" + pid;
+			document.getElementById('post' + pid + 'title').innerHTML = postIndex.list[pid].T;
+			document.getElementById('post' + pid + 'title').href = './?p=' + pid;
 		};
-		if (receivedPostText == "") {
-			document.getElementById("post" + pid + "text").remove();
+		if (receivedPostText == '') {
+			document.getElementById('post' + pid + 'text').remove();
 		} else {
-			document.getElementById("post" + pid + "text").innerHTML = receivedPostText;
+			document.getElementById('post' + pid + 'text').innerHTML = receivedPostText;
 		};
-		document.getElementById("post" + pid + "text").lang = postIndex.list[pid].Lang[1];
-		document.getElementById("post" + pid + "link").innerHTML = postIndex.list[pid].Time;
-		document.getElementById("post" + pid + "link").href = "./?p=" + pid;
-		if (postId != null && postIndex.list[pid].Title != "") {
-			document.title = postIndex.list[pid].Title.replace(/<[\/a-z]*?>/g, '') + " — " + blogName;
-		} else if (postId != null && postIndex.list[pid].Title == "") {
-			document.title = "Post #" + pid + " — " + blogName;
+		document.getElementById('post' + pid + 'link').innerHTML = postIndex.list[pid].D;
+		document.getElementById('post' + pid + 'link').href = './?p=' + pid;
+		if (postId != null && postIndex.list[pid].T != '') {
+			document.title = postIndex.list[pid].T.replace(/<[\/a-z]*?>/g, '') + ' — ' + blogName;
+		} else if (postId != null && postIndex.list[pid].T == '') {
+			document.title = 'Post #' + pid + ' — ' + blogName;
 		} else {
 			document.title = blogName;
 		};
@@ -57,27 +56,27 @@ var loadPost = function (pid) {
 };
 
 var listOutPreviousPosts = function () {
-	var section = document.createElement("section");
-	var ul = document.createElement("ul");
-	section.setAttribute("class", "post more");
-	section.setAttribute("id", "more-posts");
-	ul.setAttribute("class", "list");
+	var section = document.createElement('section');
+	var ul = document.createElement('ul');
+	section.setAttribute('class', 'post more');
+	section.setAttribute('id', 'more-posts');
+	ul.setAttribute('class', 'list');
 	for (var i = total-11; i >= 0; i--) {
-		var li = document.createElement("li");
-		var a = document.createElement("a");
-		var footer = document.createElement("footer");
-		var fa = document.createElement("a");
-		var div = document.createElement("div");
-		if (postIndex.list[i].Title == null) {
-			a.innerHTML = "[Untitled Post]";
+		var li = document.createElement('li');
+		var a = document.createElement('a');
+		var footer = document.createElement('footer');
+		var fa = document.createElement('a');
+		var div = document.createElement('div');
+		if (postIndex.list[i].T == null) {
+			a.innerHTML = '[Untitled Post]';
 		} else {
-			a.innerHTML = postIndex.list[i].Title;
+			a.innerHTML = postIndex.list[i].T;
 		};
-		li.setAttribute("lang", postIndex.list[i].Lang[0]);
-		a.setAttribute("href", "./?p=" + i);
-		a.setAttribute("class", "post-h2");
-		fa.setAttribute("href", "./?p=" + i);
-		fa.innerHTML = postIndex.list[i].Time;
+		a.setAttribute('href', './?p=' + i);
+		a.setAttribute('class', 'post-h2');
+		fa.setAttribute('href', './?p=' + i);
+		fa.innerHTML = postIndex.list[i].D;
+		footer.setAttribute('class', 'post-footer');
 		footer.appendChild(fa);
 		li.appendChild(a);
 		li.appendChild(footer);
@@ -89,43 +88,43 @@ var listOutPreviousPosts = function () {
 
 var fillNav = function (previd, nextid) {
 	if (previd != null) {
-		document.getElementById("prevlink").href = "./?p=" + previd;
-		document.getElementById("prevlink").innerHTML = "Prev »";
+		document.getElementById('prevlink').href = './?p=' + previd;
+		document.getElementById('prevlink').innerHTML = 'Prev »';
 	} else {
-		document.getElementById("prev").remove();
+		document.getElementById('prev').remove();
 	};
 	if (nextid != null) {
-		document.getElementById("nextlink").href = "./?p=" + nextid;
-		document.getElementById("nextlink").innerHTML = "« Next";
+		document.getElementById('nextlink').href = './?p=' + nextid;
+		document.getElementById('nextlink').innerHTML = '« Next';
 	} else {
-		document.getElementById("next").remove();
+		document.getElementById('next').remove();
 	};
 };
 
 var httpError = function () {
 	createSection(postId);
-	document.getElementById("post" + postId + "title").innerHTML = "404 Not Found : (";
-	document.getElementById("post" + postId + "link").remove();
-	document.getElementById("prevandnext").innerHTML = "";
-	document.title = "HTTP 404 " + "— " + blogName;
-	document.getElementById("post" + postId + "text").innerHTML = "Here is nothing you can see. Back to <em><a href='./'>homepage</a></em>?";
+	document.getElementById('post' + postId + 'title').innerHTML = '404 Not Found : (';
+	document.getElementById('post' + postId + 'link').remove();
+	document.getElementById('prevandnext').innerHTML = '';
+	document.title = 'HTTP 404 ' + '— ' + blogName;
+	document.getElementById('post' + postId + 'text').innerHTML = 'Here is nothing you can see. Back to <em><a href="./">homepage</a></em>?';
 };
 
 
 var ajaxList = new XMLHttpRequest();
-ajaxList.open("GET", "./list.json", false);
+ajaxList.open('GET', './list.json', false);
 ajaxList.send();
 var postIndex = JSON.parse(ajaxList.responseText);
 
 var total = postIndex.list.length;
-var blogName = "Joy Neop";
+var blogName = 'Joy Neop';
 var ajaxContent = [];
 
 var postId = null;
 
 var loc = window.location.href;
-if (loc.indexOf("?p=") != -1) {
-	postId = Number(loc.slice(loc.indexOf("?") + 3));
+if (loc.indexOf('?p=') != -1) {
+	postId = Number(loc.slice(loc.indexOf('?') + 3));
 };
 
 var lastPage;
@@ -133,7 +132,7 @@ var theLatestPostId = total-1;
 var loadedOldestPostId = total;
 var sectionsCreated = 0;
 
-var cont = document.getElementById("cont");
+var cont = document.getElementById('cont');
 
 if (postId != null) {
 	if (postId < total && postId >= 0) {
