f735beced5e6638b963a1fefd7b12b82e6978f76
Joy Neop <joyneop@gmail.com>
2015-06-26 07:55:51 +0000

    changed backup bg url of rainbow svg

--- js/gay-pride.js
+++ js/gay-pride.js
@@ -1,4 +1,4 @@
-(function (backgroundUrl, date, d, inputDivTag, inputStyleTag, inputAnrhorTag, defaultHeight, hoverHeight) {
+(function (backgroundUrl1, backgroundUrl2, date, d, inputDivTag, inputStyleTag, inputAnrhorTag, defaultHeight, hoverHeight) {
     if (6 <= date.getMonth() <= 7) {
         // Create <style> tag
         var styleTag = inputStyleTag || d.createElement('style');
@@ -14,7 +14,7 @@
             height: __HOVER_HEIGHT__px;\
         }\
         #gay-pride-js-anchor {\
-            background: url(__BG_URL__) no-repeat top scroll;\
+            background: url(__BG_URL_1__) no-repeat top scroll, url(__BG_URL_2__) no-repeat top scroll;\
             background-size: cover;\
             display: block;\
             position: absolute;\
@@ -32,7 +32,8 @@
             height: __HOVER_HEIGHT__px;\
             background-size: cover;\
         }\
-        '.replace(/__BG_URL__/g, backgroundUrl)
+        '.replace(/__BG_URL_1__/g, backgroundUrl1)
+        .replace(/__BG_URL_2__/g, backgroundUrl2)
         .replace(/__DEFAULT_HEIGHT__/g, defaultHeight)
         .replace(/__HOVER_HEIGHT__/g, hoverHeight);
         d.head.appendChild(styleTag);
@@ -55,7 +56,7 @@
         divTag.appendChild(aTag);
         document.body.insertBefore(divTag, document.body.firstChild);
     };
-})('/JN-Lab/other-images/rainbow-sign.svg',
+})('/JN-Lab/other-images/rainbow-sign.svg', '//joyneop.parseapp.com/JN-Lab/other-images/rainbow-sign.svg',
 (new Date()), document,
 document.getElementById('gay-pride-js-div'), document.getElementById('gay-pride-js-style-tag'), document.getElementById('gay-pride-js-anchor'),
 5, 20);
