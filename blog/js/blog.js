456a8e7b1eb827c58df3687dce6a8dd623b1d50d
Joy Neop <joyneop@gmail.com>
2015-10-08 11:45:06 +0000

    bug fix 2 with test

--- blog/js/blog.js
+++ blog/js/blog.js
@@ -4,9 +4,9 @@ window.blog = {
 
 blog.getPID = function () {
 	var PID = null;
-	var loc = location.href.match(/\=\d+$/);
+	var loc = location.href.match(/\=(\d+)$/);
 	if (loc && loc.length > 0) {
-		PID = Number(loc[0]);
+		PID = Number(loc[1]);
 		PID = isNaN(PID) ? null : PID;
 	};
 	return PID;
