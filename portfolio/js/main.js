535259d52c831c8fce74ab947874d9997ce6236d
Joy Neop <joyneop@gmail.com>
2016-02-04 16:58:56 +0000

    Implemented index page of portfolio

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -105,82 +105,49 @@ app.articleContentResponseEventHandler = function (e, targetPID) {
 	document.getElementById('post__INDEX__text'.replace(/__INDEX__/, targetPID)).innerHTML = e.target.responseText;
 };
 
-app.loadTopPosts = function () {
-	for (var i = app.articlesList.length-1; i > app.articlesList.length-11; i--) {
-		if (app.doesArticleExist(i)) {
-			app.request('./db/PID.txt'.replace(/PID/, i), app.articleContentResponseEventHandlerConstructor(i));
-		};
-	};
-};
-
 app.loadCurrentPost = function () {
 	app.request('./db/PID.txt'.replace(/PID/, app.getPID()), app.articleContentResponseEventHandlerConstructor(app.getPID()));
 };
 
 app.doesArticleExist = function (pid) {
-	console.log(pid);
-	// if (app.articlesList[String(pid)]) {
-	// 	if (app.articlesList[String(pid)].T !== 0) {
-	// 		return true;
-	// 	} else {
-	// 		return false;
-	// 	};
-	// } else {
-	// 	return false;
-	// };
-	console.log();
-	return true;
+	if (app.articlesList[String(pid)].T !== 0) {
+		return true;
+	} else {
+		return false;
+	};
 };
 
 app.main = function () {
-	window.BlogContext = document.getElementById('cont');
+	window.PortfolioContext = document.getElementById('cont');
 
-	var regularPostSectionTemplate = '<section class="post" id="post__INDEX__"><div class="post-text" id="post__INDEX__text"></div></section>';
-	var untitledPostSectionTemplate = '<section class="post" id="post__INDEX__"><div class="post-text" id="post__INDEX__text"></div></section>';
-	var listItemTemplate = '<li><a href="./?p=__INDEX__" id="post__INDEX__h2" class="post-h2">__TITLE__</a><time class="post-date"><a id="post__INDEX__date" href="./?p=__INDEX__">__DATE__</a></time></li>';
-	var listContainerTemplate = '<section class="post more" id="more-posts"><ul class="list">__CONTENT__</ul></section>';
-	var stringInBlogContext = '';
+	var regularItemEntryTemplate = '<section class="post" id="post__INDEX__"><div class="post-text" id="post__INDEX__text"></div></section>';
+	var regularItemSectionTemplate = '<li class="entry" id="entry__INDEX__"><a class="entry-anchor" id="entry__INDEX__anchor" href="./?p=__INDEX__"><img class="entry-img" src="./covers/__INDEX__.jpg"></a></li>';
+	var stringInPortfolioContext = '';
 
 	if ( app.getPID() == null ) {
 		// This is the list of posts
-		var listInBlogContext = '';
+		document.body.setAttribute('page-mode', 'index');
+		var listInPortfolioContext = '';
 
-		for (var i = app.articlesList.length-1; i > app.articlesList.length-11; i--) {
+		for (var i = app.articlesList.length-1; i >= 0; i--) {
 			if (app.doesArticleExist(i)) {
-				if (app.articlesList[String(i)].T) {
-					stringInBlogContext += regularPostSectionTemplate.replace(/__INDEX__/g, i).replace(/__TITLE__/g, app.articlesList[String(i)].T ? app.articlesList[String(i)].T : '[Untitled Post]').replace(/__DATE__/g, app.articlesList[String(i)].D);
-				} else {
-					stringInBlogContext += untitledPostSectionTemplate.replace(/__INDEX__/g, i).replace(/__TITLE__/g, app.articlesList[String(i)].T ? app.articlesList[String(i)].T : '[Untitled Post]').replace(/__DATE__/g, app.articlesList[String(i)].D);
-				};
+				stringInPortfolioContext += regularItemSectionTemplate.replace(/__INDEX__/g, i).replace(/__TITLE__/g, app.articlesList[String(i)].T ? app.articlesList[String(i)].T : '[Untitled Post]').replace(/__DATE__/g, app.articlesList[String(i)].D);
 			};
 		};
-		for (var i = app.articlesList.length-1-11; i > -1; i--) {
-			if (app.doesArticleExist(i)) {
-				listInBlogContext += listItemTemplate.replace(/__INDEX__/g, i).replace(/__TITLE__/g, app.articlesList[String(i)].T ? app.articlesList[String(i)].T : '[Untitled Post]').replace(/__DATE__/g, app.articlesList[String(i)].D);
-			};
-		};
-		stringInBlogContext += listContainerTemplate.replace(/__CONTENT__/g, listInBlogContext);
-		BlogContext.innerHTML = stringInBlogContext;
-		app.loadTopPosts();
+		PortfolioContext.innerHTML = stringInPortfolioContext;
 	} else {
 		// This is a particular post
+		document.body.setAttribute('page-mode', 'single');
 		if ( 0 <= app.getPID() && app.getPID() < app.articlesList.length && app.doesArticleExist(app.getPID())) {
 			// This is a valid URL for a post
-			var postTitle;
-			if (app.articlesList[String(app.getPID())].T) {
-				postTitle = app.articlesList[String(app.getPID())].T;
-				stringInBlogContext = regularPostSectionTemplate.replace(/__INDEX__/g, app.getPID()).replace(/__TITLE__/g, app.articlesList[String(app.getPID())].T).replace(/__DATE__/g, app.articlesList[app.getPID()].D);
-			} else {
-				postTitle = '[Untitled Post]';
-				stringInBlogContext = untitledPostSectionTemplate.replace(/__INDEX__/g, app.getPID()).replace(/__TITLE__/g, app.articlesList[String(app.getPID())].T).replace(/__DATE__/g, app.articlesList[app.getPID()].D);
-			};
-			document.title = postTitle + ' — Joy Neop';
-			BlogContext.innerHTML = stringInBlogContext;
+			stringInPortfolioContext = regularItemEntryTemplate.replace(/__INDEX__/g, app.getPID()).replace(/__DATE__/g, app.articlesList[app.getPID()].D);
+			document.title = app.articlesList[String(app.getPID())].T + ' — Joy Neop';
+			PortfolioContext.innerHTML = stringInPortfolioContext;
 			app.loadCurrentPost(app.getPID());
 		} else {
 			// This post should not exist
-			stringInBlogContext = regularPostSectionTemplate.replace(/__INDEX__/g, app.getPID()).replace(/__TITLE__/g, '404 Not Found');
-			BlogContext.innerHTML = stringInBlogContext;
+			stringInPortfolioContext = regularItemSectionTemplate.replace(/__INDEX__/g, app.getPID()).replace(/__TITLE__/g, '404 Not Found');
+			PortfolioContext.innerHTML = stringInPortfolioContext;
 			document.getElementById('post__PID__text'.replace(/__PID__/, app.getPID())).innerHTML = '<p>The post does not exist : (</p>';
 			document.getElementById('post__PID__link'.replace(/__PID__/, app.getPID())).remove();
 		};
