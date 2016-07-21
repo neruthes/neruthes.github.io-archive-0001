bee8de6064c8ee05e17e2092aa718aa6eee2e8fe
Joy Neop <joyneop@gmail.com>
2016-07-21 15:09:37 +0000

    Brave new /color/

--- portfolio/portfolio_sitemap.js
+++ portfolio/portfolio_sitemap.js
@@ -6,21 +6,31 @@ Run on Node.js
 
 */
 
-var blogPath = __dirname;
-var postTemplate = '\n<url>\n\t<loc>__LOC__</loc>\n\t<lastmod>__MOD__</lastmod>\n\t<priority>0.8</priority>\n</url>\n';
+var thisDirPath = __dirname;
+var postTemplate = '\n<url>\n\t<loc>__LOC__</loc>\n\t<priority>0.8</priority>\n</url>\n';
 var listJson;
 var accumulate = '';
 
 var fs = require('fs');
 
-listJson = JSON.parse(fs.readFileSync(blogPath + '/list.json', 'utf8').toString());
+listJson = JSON.parse(fs.readFileSync(thisDirPath + '/list.json', 'utf8').toString());
+listJson.length = (function (list) {
+    var count = 0;
+    var i;
+    for (i in list) {
+        if (list.hasOwnProperty(i)) {
+            count++;
+        };
+    };
+    return count;
+})(listJson);
 
-var maxId = 220;
+var maxId = listJson.length-1;
 for (var i = 0; i <= maxId && i < listJson.length; i++) {
-	accumulate = postTemplate.replace(/__LOC__/g, 'https://joyneop.xyz/portfolio/?p=' + i).replace(/__MOD__/g, listJson.list[i]['D']) + accumulate;
+	accumulate = postTemplate.replace(/__LOC__/g, 'https://joyneop.xyz/portfolio/?p=' + i) + accumulate;
 };
 
 
-fs.writeFileSync(blogPath + '/blog_sitemap.xml', accumulate);
+fs.writeFileSync(thisDirPath + '/portfolio_sitemap.xml', accumulate);
 
 console.log('Done!');
